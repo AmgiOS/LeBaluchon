91725db4bce0319b9e191688d9b92561f9555b85
hakimamg <hakimdu9347@gmail.com>
2018-08-23 01:45:28 +0200

    Design Application Projet 9

---
+++ Projet 9/Controller/TranslateViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TranslateViewController.swift
+//  Projet 9
+//
+//  Created by Amg on 23/08/2018.
+//  Copyright © 2018 Amg-Industries. All rights reserved.
+//
+
+import UIKit
+
+class TranslateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
